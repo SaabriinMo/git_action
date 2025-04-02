3e3c2c280ed4c6e30b2a7c62328daac34c63b3b7
SaabriinMo <saabriinm123@gmail.com>
2025-04-02 17:00:35 +0100

    amending test file

--- main.py
+++ main.py
@@ -2,23 +2,25 @@ import requests
 
 
 URL = "https://genshin.jmp.blue/"
-HEADER = {'Accept': 'application/json'}
+HEADER = {"Accept": "application/json"}
 # status_code = requests.get("https://genshin.jmp.blue/", headers={'Accept': 'application/json'})
 
 # print(status_code.json())
+
 
 def getting_status_code(url):
     return requests.get(url)
 
 
 def get_genshin_data():
-    response = requests.get(URL, headers={'Accept': 'application/json'})
+    response = requests.get(URL, headers={"Accept": "application/json"})
     if response:
         return response.json()
     return None
 
+
 def get_character_data():
-    response = requests.get(URL+"characters", headers={'Accept': 'application/json'})
+    response = requests.get(URL + "characters", headers={"Accept": "application/json"})
     if response:
         return response.json()
     return None
--- test_main.py
+++ test_main.py
@@ -1,6 +1,7 @@
 import main
 import requests
 
+
 def test_get_list_of_users():
     url = "https://genshin.jmp.blue/"
     response = requests.get(url)
